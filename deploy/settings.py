81323e051a9c3e8280d46e040a113cdbeadb1097
Akanksha <jaiswal.akanksha0@gmail.com>
2022-10-26 17:47:46 +0530

    thirdcommit

--- deploy/settings.py
+++ deploy/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u6%*hg=z#n!j^@&=z@87(0x@bs_3hgnh1xrp1uar@2ij1mfx8z'
+SECRET_KEY = os.environ['abc']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
